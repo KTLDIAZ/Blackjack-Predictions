0ef5a6d6d490dc7dbc95bcbc95b90c7134bb78fd
Melvin Diaz <melvindiaz99@gmail.com>
2021-03-28 15:44:20 -0600

    Jupviewer tittle updated

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ const App = () => (
         </Route>
         <Route path="/notebook">
           <JupViewer
-            title="Jupyter as a Blog!"
-            subtitle="I've always wanted to publish my jupyter notebooks as blogs. Finally I can."
+            title="Blackjack Predictions"
+            subtitle=""
             // coverImg="https://notionpress.com/blog/wp-content/uploads/2018/06/Cover-design.png"
             file={ipynb}
             // file="https://raw.githubusercontent.com/fastai/course-v3/master/nbs/dl1/00_notebook_tutorial.ipynb"
